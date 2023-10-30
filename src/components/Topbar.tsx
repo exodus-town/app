f9f6baeaadeee19e54f7898d4252b839e0486846
contribuyente <monotributista.contribuyente@proton.me>
2023-10-30 18:42:46 -0300

    jump in

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -5,13 +5,12 @@ import { MdOutlineArrowBackIosNew as Back } from 'react-icons/md'
 import { Button } from "decentraland-ui"
 import { useAccount } from "wagmi"
 import { toCoords } from "../lib/coords"
-import { useToken } from "../modules/token"
+import { toLayout } from "../lib/layout"
 import { useLogin } from "../modules/login"
 import './Topbar.css'
 
 export const Topbar = memo(() => {
   const { tokenId } = useParams()
-  const { isOwner } = useToken(tokenId)
   const { isConnected } = useAccount()
   const { login, isLoggingIn } = useLogin()
 
@@ -19,10 +18,14 @@ export const Topbar = memo(() => {
     return tokenId ? toCoords(tokenId) : [0, 0] as const
   }, [tokenId])
 
+  const { base } = useMemo(() => {
+    return tokenId ? toLayout(tokenId) : { base: { x: 0, y: 0 } }
+  }, [tokenId])
+
   return <div className="Topbar">
     <div className="title"><Link to="/"><Back className="back" /> Parcel {x},{y}</Link></div>
     <div className="actions">
-      {isConnected ? <><Button href={`https://play.decentraland.org?realm=exodus.town/tokens/${tokenId}`} secondary size="small" target="_blank">Preview</Button>{isOwner ? <Button primary size="small">Publish</Button> : null}</> : <Button primary size="small" onClick={login} disabled={isLoggingIn} loading={isLoggingIn}>Sign In</Button>}
+      {isConnected ? <Button href={`https://play.decentraland.org?realm=exodus.town&position=${base.x},${base.y}`} primary size="small" target="_blank">Jump In</Button> : <Button primary size="small" onClick={login} disabled={isLoggingIn} loading={isLoggingIn}>Sign In</Button>}
     </div>
   </div>
 })
