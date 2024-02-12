98c85013b0f30d9f9a027aa77024576984fc76a3
contribuyente <monotributista.contribuyente@proton.me>
2024-02-12 17:02:30 -0300

    remove unused files

--- functions/cache/entities.ts
+++
@@ -1,8 +0,0 @@
-import { getEntityMappings } from "../lib/entity";
-import { Env } from "../lib/env";
-import { json } from "../lib/response";
-
-export const onRequestGet: PagesFunction<Env> = async (context) => {
-  const entites = await getEntityMappings(context.env);
-  return json(entites);
-};
