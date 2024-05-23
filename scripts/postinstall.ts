63835f42632636b1ed5ffae5c297b48d2f9f7dee
contribuyente <monotributista.contribuyente@proton.me>
2024-05-23 15:34:08 -0300

    log version

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -2,14 +2,18 @@ import fs from "fs";
 import path from "path";
 
 // Copy inspector assets to the public folder
-const inspectorAssetsPath = path.resolve(
-  "./node_modules/@dcl/inspector/public"
-);
+const inspectorPath = path.resolve("./node_modules/@dcl/inspector");
+const inspectorAssetsPath = path.resolve(inspectorPath, "./public");
+const inspectorPackagePath = path.resolve(inspectorPath, "./package.json");
 
 console.log("Inspector assets:", inspectorAssetsPath);
 const files = fs.readdirSync(inspectorAssetsPath);
 const publicPath = path.resolve("./public");
 console.log("Public folder:", publicPath);
+console.log(
+  "Inspector version:",
+  JSON.parse(fs.readFileSync(inspectorPackagePath, "utf-8")).version
+);
 for (const file of files) {
   const source = path.resolve(inspectorAssetsPath, file);
   if (file.endsWith(".map")) continue;
